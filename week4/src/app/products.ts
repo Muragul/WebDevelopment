79c4662c100bda0ccb5c5ad5d857a85487c30480
Muratbekova Muragul <47049297+Muragul@users.noreply.github.com>
2020-02-12 23:20:41 +0600

    [finished Aliexpress. Angular]

---
+++ week4/src/app/products.ts
@@ -0,0 +1,102 @@
+export const products = [
+    {
+      name: 'LANBENA eye patches',
+      description: 'Eye patches eye mask hyaluronic acid collagen eye patch skin care gel with hyaluronic acid moisturizing retinol anti-aging removes dark circles eye bag',
+      rate: 4.9,
+      image: '../assets/1.png',
+      img1: '../assets/11.png',
+      img2: '../assets/12.png',
+      img3: '../assets/13.png',
+      link: 'https://aliexpress.ru/item/4000467213259.html?spm=a2g0o.productlist.0.0.746544a5RShhlG&s=p&algo_pvid=5cbcf258-3073-4268-9245-df199c4a28ab&algo_expid=5cbcf258-3073-4268-9245-df199c4a28ab-0&btsid=2e97e243-ec60-4df2-8623-ae612c1762e5&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    },
+    {
+      name: 'LAIKOU',
+      description: 'Silicone Facial Cleansing Brush, Electric Facial Cleanser, Electric Skin Cleanser, Deep Wash Massage Brush',
+      rate: 4.9,
+      image: '../assets/2.png',
+      img1: '../assets/21.png',
+      img2: '../assets/22.png',
+      img3: '../assets/23.png',
+      link: 'https://aliexpress.ru/item/4000283906248.html?spm=a2g0o.productlist.0.0.746544a5RShhlG&algo_pvid=5cbcf258-3073-4268-9245-df199c4a28ab&algo_expid=5cbcf258-3073-4268-9245-df199c4a28ab-10&btsid=2e97e243-ec60-4df2-8623-ae612c1762e5&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    },
+    {
+      name: 'FOCALLURE',
+      description: '19 colors, matte waterproof lipstick, matte lipstick, lipstick, cosmetics, easy to wear, matte matte lipstick Batom',
+      rate: 4.7,
+      image: '../assets/3.png',
+      img1: '../assets/31.png',
+      img2: '../assets/32.png',
+      img3: '../assets/33.png',
+      link: 'https://aliexpress.ru/item/32795272202.html?spm=a2g0o.productlist.0.0.462170b9XsVmqM&s=p&algo_pvid=31e9afb8-e455-4065-bac5-b108792e66ef&algo_expid=31e9afb8-e455-4065-bac5-b108792e66ef-4&btsid=f6d15227-3c75-4f06-a38d-a95d34b6b38f&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53' 
+    },
+    {
+      name: 'MISSHA eye concealer',
+      description: 'Face makeup cream style under the eyes Brightening BB cream Korean cosmetics',
+      rate: '5.0',
+      image: '../assets/4.png',
+      img1: '../assets/41.png',
+      img2: '../assets/42.png',
+      img3: '../assets/43.png',
+      link: 'https://aliexpress.ru/item/32952799736.html?spm=a2g0o.productlist.0.0.462170b9XsVmqM&algo_pvid=31e9afb8-e455-4065-bac5-b108792e66ef&algo_expid=31e9afb8-e455-4065-bac5-b108792e66ef-39&btsid=f6d15227-3c75-4f06-a38d-a95d34b6b38f&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    },
+    {
+      name: 'HANDAIYAN',
+      description: 'Holographic Mermaid Glitter Liquid Eye Shadow Body Face Eye Liquid Glitter Pigment Free Face Contour Party Festival Paste',
+      rate: 4.8,
+      image: '../assets/5.png',
+      img1: '../assets/51.png',
+      img2: '../assets/52.png',
+      img3: '../assets/53.png',
+      link: 'https://aliexpress.ru/item/4000020091444.html?spm=a2g0o.productlist.0.0.462170b9XsVmqM&algo_pvid=31e9afb8-e455-4065-bac5-b108792e66ef&algo_expid=31e9afb8-e455-4065-bac5-b108792e66ef-46&btsid=f6d15227-3c75-4f06-a38d-a95d34b6b38f&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    },
+    {
+      name: 'FLD Professional Makeup Brush Set',
+      description: 'A set of brushes for powder and blush with crystals, a set of brushes for eyeliner, eyebrows, a set of tools for makeup',
+      rate: 4.8,
+      image: '../assets/6.png',
+      img1: '../assets/61.png',
+      img2: '../assets/62.png',
+      img3: '../assets/63.png',
+      link: 'https://aliexpress.ru/item/33044758385.html?spm=a2g0o.productlist.0.0.64282c9bcwtUr3&algo_pvid=801e224d-7595-493d-be40-e35015093122&algo_expid=801e224d-7595-493d-be40-e35015093122-43&btsid=f9629f7a-e1ad-40ff-a652-a4b2692e016a&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    },
+    {
+      name: 'Body Shimmer',
+      description: 'Liquid glossy lips The face of the body can illuminate the brightness of the face for long-lasting radiance of the face',
+      rate: 4.7,
+      image: '../assets/7.png',
+      img1: '../assets/71.png',
+      img2: '../assets/72.png',
+      img3: '../assets/73.png',
+      link: 'https://aliexpress.ru/item/4000009197341.html?spm=a2g0o.productlist.0.0.23c34c08OX5g9x&algo_pvid=c5f841d0-bbc5-4b13-a2fc-a6d07052dba2&algo_expid=c5f841d0-bbc5-4b13-a2fc-a6d07052dba2-8&btsid=7544c809-dbee-4909-ac2e-f7376e0f1428&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53' 
+    },
+    {
+      name: 'Galaxy Star',
+      description: 'Makeup spray long-term installation moisturizing, moisturizing oil control matte finish spray cosmetic primer spray 100 ml',
+      rate: 4.9,
+      image: '../assets/8.png',
+      img1: '../assets/81.png',
+      img2: '../assets/82.png',
+      img3: '../assets/83.png',
+      link: 'https://aliexpress.ru/item/4000224121352.html?spm=a2g0o.productlist.0.0.23c34c08OX5g9x&algo_pvid=c5f841d0-bbc5-4b13-a2fc-a6d07052dba2&algo_expid=c5f841d0-bbc5-4b13-a2fc-a6d07052dba2-57&btsid=7544c809-dbee-4909-ac2e-f7376e0f1428&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    },
+    {
+      name: 'Miss Rose',
+      description: 'Face powder, makeup kit, matte and shimmering eyeshadow palette, blockbuster, professional makeup kit, highlighter, bronzer',
+      rate: 4.7,
+      image: '../assets/9.png',
+      img1: '../assets/91.png',
+      img2: '../assets/92.png',
+      img3: '../assets/93.png',
+      link: 'https://aliexpress.ru/item/32809818347.html?spm=a2g0o.productlist.0.0.31c775c9NmQwTR&algo_pvid=f3524f73-8133-435e-8b92-6585792cb077&algo_expid=f3524f73-8133-435e-8b92-6585792cb077-9&btsid=d6d1d190-d65b-49a3-869b-765dbbda8e86&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    },
+    {
+      name: 'Baking Powder Scrub',
+      description: 'Baking powder, pore cleansing foam 160 ml, facial cleanser, shrinkable porous skin, deep cleansing care, whitening scrub, Korean cosmetics',
+      rate: 4.9,
+      image: '../assets/10.png',
+      img1: '../assets/101.png',
+      img2: '../assets/102.png',
+      img3: '../assets/103.png',
+      link: 'https://aliexpress.ru/item/32766627073.html?spm=a2g0o.productlist.0.0.6d4f44a5UBM2bT&algo_pvid=3ff4b3a2-fd94-4174-bb01-a91780fa6fa9&algo_expid=3ff4b3a2-fd94-4174-bb01-a91780fa6fa9-46&btsid=8ef9eb1c-5338-4a63-ba55-a33eabe298d5&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_53'
+    }
+  ];
