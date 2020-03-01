cfa62ec486e3d6f7df7ef50c9bd52f9211dbeaa9
Muragul <muragulm@gmail.com>
2020-03-01 23:21:13 +0600

    passed midterm

---
+++ midterm/muragul/src/app/jobs.ts
@@ -0,0 +1,52 @@
+import { Job } from './job';
+
+export const JOBS: Job [] = [
+    {
+        id: 1,
+        title: 'Teacher',
+        description: 'KBTU. Web Development, Frondend',
+        like_count: 0
+    },
+    {
+        id: 2,
+        title: 'Surger',
+        description: 'Halyk Surgery. Full day',
+        like_count: 0
+    },
+    {
+        id: 3,
+        title: 'Dentist',
+        description: 'DostarMed. Dentist. Full day',
+        like_count: 0
+    },
+    {
+        id: 4,
+        title: 'Singer',
+        description: 'MuzCafe. Every friday evening',
+        like_count: 0
+    },
+    {
+        id: 5,
+        title: 'Shief',
+        description: 'Ocean basket. Dessert shief',
+        like_count: 0
+    },
+    {
+        id: 6,
+        title: 'Project Manager',
+        description: 'BiGroup. WebDevelopment team',
+        like_count: 0
+    },
+    {
+        id: 7,
+        title: 'Android Developer',
+        description: 'Codebusters_. Android team',
+        like_count: 0
+    },
+    {
+        id: 8,
+        title: 'Finance',
+        description: 'Big Four. Detloite company. Full day',
+        like_count: 0
+    }
+]
