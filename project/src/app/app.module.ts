d7b8bf1763076fb7671b784b3f15d96f0c581f14
Muragul <muragulm@gmail.com>
2020-03-20 13:14:33 +0600

    [pull]: navbar

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { MainListComponent } from './main-list/main-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       SignUpComponent,
       LogInComponent,
-      MainListComponent
+      MainListComponent,
+      NavbarComponent
    ],
    imports: [
       BrowserModule,
