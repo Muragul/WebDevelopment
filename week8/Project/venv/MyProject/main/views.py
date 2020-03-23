b39f4417f2de04cb8b3ee114b7cd9364c79a2f09
Muragul <muragulm@gmail.com>
2020-03-24 03:59:26 +0600

    practice on django

--- week8/Project/venv/MyProject/main/views.py
+++ week8/Project/venv/MyProject/main/views.py
@@ -1,5 +1,24 @@
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
+
 
 def hello(request):
-    return HttpResponse('hello msg')
+    return HttpResponse('<h1>hello msg</h1>')
+
+products = [
+    {
+        'id': i,
+        'name': f'product {i}',
+        'price': i * 1000
+    } for i in range(1, 5)
+]
+
+
+def product_list(request):
+    return JsonResponse(products, safe=False)
+
+
+def product_detail(request, product_id):
+    for p in products:
+        if p['id']==product_id:
+            return JsonResponse(p)
+    return JsonResponse({'error': 'does not exist'})
