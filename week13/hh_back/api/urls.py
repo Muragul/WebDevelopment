9d3e9a46252f947df7d6225bc19f6c5a0cb279a6
Muragul <muragulm@gmail.com>
2020-04-15 04:03:12 +0600

    finished functional

---
+++ week13/hh_back/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
+from api.views.fbv import company_list, company_detail, company_vacancies, vacancy_list, vacancy_detail
+from api.views.cbv import CompanyList, VacancyList, CompanyVacancies, VacancyDetails, CompanyDetails
+
+urlpatterns = [
+    path('login/', obtain_jwt_token),
+    path('companies/', CompanyList.as_view()),
+    path('companies/<int:pk>/', CompanyDetails.as_view()),
+    path('companies/<int:company_id>/vacancies/', CompanyVacancies.as_view()),
+    path('vacancies/', VacancyList.as_view()),
+    path('vacancies/<int:pk>/', VacancyDetails.as_view())
+]
+# urlpatterns = [
+#     path('companies/', company_list),
+#     path('companies/<int:company_id>/', company_detail),
+#     path('companies/<int:company_id>/vacancies/', company_vacancies),
+#     path('vacancies/', vacancy_list),
+#     path('vacancies/<int:vacancy_id>/', vacancy_detail),
+# ]
